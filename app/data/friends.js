dcee4ac191a0fa7d0261cfca0f7d5ce8fda1dbe1
Moshiur <moshiur@gmail.com>
2019-01-14 12:00:42 -0500

    Update

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+
+var friends = [
+    {
+        "name":"Owen Miller",
+        "photo":"https://moshiurrah.github.io/FriendFinder/app/assets/images/mhs_001.jpg",
+        "scores":[1, 2, 4, 2, 5, 2, 1, 3, 1, 2]
+    },
+    {
+      "name": "Kimmy Schmidt",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/fmhs_001.jpg",
+      "scores": [4, 4, 2, 5, 3, 4, 5, 2, 3, 4]
+    },    
+    {
+      "name": "Dexter Morgan",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/mhs_002.jpg",
+      "scores": [3, 2, 5, 4, 1, 1, 4, 2, 3, 2]
+    },
+    {
+      "name": "Maggie Rhee",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/fmhs_002.jpg",
+      "scores": [3, 2, 3, 1, 3, 2, 3, 1, 3, 5]
+    },    
+    {
+      "name": "Sam Winchester",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/mhs_003.jpg",
+      "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+      "name": "Emma Wilkins",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/fmhs_003.jpg",
+      "scores": [1, 4, 5, 2, 1, 3, 1, 5, 2, 4]
+    },      
+    {
+      "name": "Tom Winchester",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/mhs_004.jpg",
+      "scores": [4, 5, 4, 5, 4, 5, 4, 5, 4, 5]
+    },
+    {
+      "name": "Sue Wilkins",
+      "photo": "https://moshiurrah.github.io/FriendFinder/app/assets/images/fmhs_004.jpg",
+      "scores": [1, 3, 1, 2, 4, 4, 1, 5, 5, 4]
+    },
+  ]
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
